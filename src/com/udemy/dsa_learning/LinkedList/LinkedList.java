e4b777a4f732297d9f088780ec48b48467b04e04
TejaSaii <tejas7833@gmail.com>
2023-03-04 15:47:40 +0530

    linked list'

--- src/com/udemy/dsa_learning/LinkedList/LinkedList.java
+++ src/com/udemy/dsa_learning/LinkedList/LinkedList.java
@@ -5,38 +5,38 @@ public class LinkedList {
 	private Node head;
 	private Node tail;
 	private int length;
-	
+
 	public LinkedList(int value) {
 		Node newNode = new Node(value);
 		this.head = newNode;
 		this.tail = newNode;
 		length = 1;
 	}
-	
+
 	public class Node {
 		int value;
 		Node next;
-		
+
 		Node(int value){
 			this.value = value;
 		}
 	}
-	
+
 	public void printList(){
 		Node temp = head;
 		while(temp != null){
-			 System.out.println(temp.value);
-			 temp = temp.next;
+			System.out.println(temp.value);
+			temp = temp.next;
 		}
 	}
-	
+
 	public void prepend(int value) {
 		Node newNode = new Node(value);
 		newNode.next=head;
 		head = newNode;
 		length++;
 	}
-	
+
 	public void append(int value) {
 		Node newNode = new Node(value);
 		if(length == 0) {
@@ -49,15 +49,41 @@ public class LinkedList {
 		}
 		length++;
 	}
-	
+
+	public Node removeLast() {
+		//case 1: when list is empty
+		if(length == 0) return null;
+		
+		//case 2: when list has elements
+		Node temp = head;
+		Node pre = head;
+		while(temp.next != null) {
+			pre = temp;
+			temp = temp.next;
+		}
+		tail = pre;
+		pre.next = null;
+		length--;
+		
+		//case 3: when list has single element
+		//covering the edge case where list has only one item
+		//and when it goes through the above code it sets the length as 0 but the Node still exists
+		if(length == 0) {
+			head = null;
+			tail = null;
+		}
+		//returning the removed element
+		return temp;
+	}
+
 	public void getHead() {
 		System.out.println("Head: "+head.value);
 	}
-	
+
 	public void getTail() {
 		System.out.println("Tail: "+tail.value);
 	}
-	
+
 	public void getLength() {
 		System.out.println("Length: "+length);
 	}
--- src/com/udemy/dsa_learning/LinkedList/Main.java
+++ src/com/udemy/dsa_learning/LinkedList/Main.java
@@ -4,16 +4,24 @@ public class Main {
 	
 	public static void main(String[] args) {
 		LinkedList myList = new LinkedList(1);
-		myList.printList();
-		myList.getHead();
-		myList.getTail();
-		myList.getLength();
+		myList.append(2);
+		myList.prepend(3);
+		myList.append(4);
 		
-		myList.prepend(2);
-		myList.append(3);
-		myList.getLength();
 		myList.printList();
 		myList.getHead();
 		myList.getTail();
+		
+		//1 - removing 4
+		System.out.println(myList.removeLast().value);
+		//2 - removing 2
+		System.out.println(myList.removeLast().value);
+		//3 - removing 1
+		System.out.println(myList.removeLast().value);
+		//4 - removing last element
+		System.out.println(myList.removeLast().value);
+		System.out.println(myList.removeLast());
+		
+		
 	}
 }
