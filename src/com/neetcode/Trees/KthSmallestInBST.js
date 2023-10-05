f2f8d9100c923212d2c9f4d2527edde85774ac69
TejaSaii <tejas7833@gmail.com>
2023-10-05 11:02:08 +0530

    Added iterative sol for Kth smallest in BST

--- src/com/neetcode/Trees/KthSmallestInBST.js
+++ src/com/neetcode/Trees/KthSmallestInBST.js
@@ -26,3 +26,36 @@ const dfs = (root, k, stack) => {
 
     return stack[(k - 1)];
 };
+
+//iterative in-Order traversal
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function (root, k, stack = []) {
+    while (k--) {
+        root = moveLeft(root, stack);
+
+        if (k === 0) return root.val;
+        
+        root = root.right;
+    }
+};
+
+const moveLeft = (root, stack) => {
+    while (root) {
+        stack.push(root);
+        root = root.left;
+    }
+
+    return stack.pop();
+}
