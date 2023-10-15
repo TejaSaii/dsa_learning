0c056a6d4748c5ba2b2c6980c0677505433ed6ee
TejaSaii <tejas7833@gmail.com>
2023-10-15 13:20:23 +0530

    Solved TaskScheduler with maxHeap and queue

--- src/com/neetcode/HeapsOrPriorityQueues/TaskScheduler.js
+++ src/com/neetcode/HeapsOrPriorityQueues/TaskScheduler.js
@@ -39,3 +39,48 @@ const getMostFrequent = (maxFrequency, frequencyMap, mostFrequent = 0) => {
 
     return mostFrequent;
 }
+
+//Solution with MAX HEAP and a QUEUE
+/**
+ * @param {character[]} tasks
+ * @param {number} n
+ * @return {number}
+ */
+var leastInterval = function(tasks, n) {
+    const freqMap = getFrequencyMap(tasks);
+    const maxHeap = new MaxPriorityQueue();
+    const q = [];
+	
+    //adding the counts into the heap
+    freqMap.forEach((e) => {
+        if(e) maxHeap.enqueue(e);
+    });
+
+    let time = 0;
+    while(!maxHeap.isEmpty() || q.length) {
+        time++;
+
+	//if heap is not empty take the max count and decrement it by 1
+	//if the value is !== 0 we add it to the queue by the next time it can be used
+        if(!maxHeap.isEmpty()) {
+            let count = maxHeap.dequeue().element - 1;
+            if(count) {
+                q.push( [count, time + n] );
+            }
+        }
+	//if there is an entry in queue which is equal to time we pop it from queue and add to heap
+        if(q.length && q[0][1] === time) {
+            maxHeap.enqueue( q.shift()[0] );
+        }
+    }
+
+    return time;
+};
+
+function getFrequencyMap(tasks, freqMap = new Array(26).fill(0)) {
+    for(const task of tasks) {
+        const index = task.charCodeAt(0) - 'A'.charCodeAt(0);
+        freqMap[index]++;
+    }
+    return freqMap;
+}
