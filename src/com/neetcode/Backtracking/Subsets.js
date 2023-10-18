058dfa23fa7307bc4911069c3b48b96b116db32a
TejaSaii <tejas7833@gmail.com>
2023-10-18 09:05:37 +0530

    Solved Subsets using recursion

---
+++ src/com/neetcode/Backtracking/Subsets.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+const subsets = (nums) => {
+    const res = [];
+    const subset = [];
+    const dfs = (i) => {
+      if(i >= nums.length)
+        return res.push([...subset]);
+    
+      subset.push(nums[i]);
+      dfs(i + 1);
+      subset.pop();
+      dfs(i + 1);
+    }
+    
+    dfs(0);
+
+    return res;
+}
+
