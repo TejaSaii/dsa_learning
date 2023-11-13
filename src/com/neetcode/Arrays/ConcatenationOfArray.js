8b6677d3f40737e2f59472c56bc64d980cb652cf
TejaSaii <tejas7833@gmail.com>
2023-11-13 11:53:38 +0530

    Started solving arrays from neetcode 150

---
+++ src/com/neetcode/Arrays/ConcatenationOfArray.js
@@ -0,0 +1,5 @@
+var getConcatenation = (nums) => {
+  return nums.concat(nums);  
+}
+
+console.log(getConcatenation([1, 3, 2, 1]));
---
+++ src/com/neetcode/Practice/KDivisibleElementsSubarrays.js
@@ -0,0 +1,18 @@
+/*Given an integer array nums and two integers k and p, return the number of distinct subarrays, which have at most k elements that are divisible by p.
+
+Two arrays nums1 and nums2 are said to be distinct if:
+
+They are of different lengths, or
+There exists at least one index i where nums1[i] != nums2[i].
+A subarray is defined as a non-empty contiguous sequence of elements in an array.
+*/
+
+const getSubArrays = (nums, k, p) => {
+  let count = 0;
+  const res = [];
+  for(let l = 0; l < nums.length; l++) {
+    for(let r = l; r < nums.length; r++) {
+      
+    }
+  }
+}
